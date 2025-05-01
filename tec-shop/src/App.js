9f502d059c594242cc99ad55903ae4e44b79d981
Jonathan Rosero <jars9605@gmail.com>
2025-04-30 21:07:29 -0500

    Update App.js

--- tec-shop/src/App.js
+++ tec-shop/src/App.js
@@ -1,18 +1,43 @@
 import HelloWorld from "./components/HelloWorld";
-
+import ButtonHook from "./components/ButtonHook";
 function App() {
+  const hello = "SALUDOS";
+  // const person = {
+  //   name: "Jonathan Rosero",
+  //   age: 29,
+  //   job: "Ing. Sistemas",
+  // };
 
-  const hello = "Saludos";
+  const persons = [
+    { name: "Jonathan Rosero", age: 29, job: "Ing. Sistemas" },
+    { name: "Jorge Hurtado", age: 30, job: "Florista" },
+    { name: "Jessica Condoy", age: 25, job: "Médico", hobbie: "Leer" },
+    {
+      name: "Matilde Ramón",
+      age: 25,
+      job: "Electrónico",
+      address: "Loja, Loja, Las Pitas",
+    },
+    {
+      name: "Matilde Ramón",
+      age: 25,
+      job: "Electrónico",
+      address: "Loja, Loja, Las Pitas",
+    },
+  ];
 
-  const person = {
-    name: "Jonathan Rosero",
-    age: 28,
-    title: "Msc. Ingeniería de Software",
-    job: "Especialista TI / FULLSTACK DEVELOPER",
-  }
+  // console.log(persons)
   return (
     <div>
-      <HelloWorld greeting={hello} person={person}/>
+      <h1>{hello}</h1>
+      {/* {hello} {person.name} tu edad es: {person.age} */}
+      {/* <HelloWorld greeting={hello} person={person}/> */}
+      {persons.map((persona) => (
+        <div key={persona.name}>
+          <HelloWorld person={persona}/>
+        </div>
+      ))}
+      <ButtonHook></ButtonHook>
     </div>
   );
 }
